fc3e774af74e5e99152d06d16740a6e256bddace
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-06 12:02:26 -0300

    implemented request loan feature

--- script.js
+++ script.js
@@ -176,6 +176,19 @@ btnTransfer.addEventListener('click', event => {
   }
 });
 
+btnLoan.addEventListener('click', event => {
+  event.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', event => {
   event.preventDefault();
 
